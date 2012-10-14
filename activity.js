80bfdfac0a73aa766f2adc14dd141665c1c30e80
Will Thames <will@thames.id.au>
2012-10-14 14:16:06 +1000

    Modularisation working better - abstracted graph drawing to just drawGraph

--- activity.js
+++ activity.js
@@ -1,3 +1,4 @@
+
 function json_to_records(json) { 
     var records = json.records;
     
@@ -9,29 +10,70 @@ function json_to_records(json) {
     return records;
 }
 
-function json_to_laps(json) { 
+function json_to_laps(json, xValue) { 
     var laps = [];
     for (var i = 0, n = json.laps.length; i < n; i++) { 
         var x0 = (i==0 ? 0 : laps[i-1].x1)
         var lap = {
             x0: x0,
-            x1: +json.laps[i].d + x0
+            x1: +xValue(json.laps[i]) + x0
         };
         laps.push(lap);
     }
     return laps;
 }
 
-function velocityGraph(records, width, height, by_distance = true) { 
+function drawGraph(json, xValue, yValue, 
+                   width, height) { 
 
+    var m = [0.1 * width, 0.1 * height, 0.1 * width, 0.1 * height];
+    var x = d3.scale.linear().range([0, width]),
+        y = d3.scale.linear().range([height,0]),
+        xAxis = d3.svg.axis().scale(x),
+        yAxis = d3.svg.axis().scale(y).orient("left");
+
+    records = json_to_records(json);
+    laps = json_to_laps(json, xValue);
+    
+    x.domain([0, xValue(records[records.length - 1])]);
+    y.domain([0, 1.1*d3.max(records, yValue)]);
+
+    var svg = d3.select("body").append("svg:svg")
+        .attr("width", width + m[1] + m[3])
+        .attr("height", height + m[0] + m[2])
+      .append("svg:g")
+        .attr("transform", "translate(" + m[3] + "," + m[0] +")");
+
+    // Add the laps
+    svg.selectAll(".fish")
+        .data(laps)
+      .enter().append("svg:rect")
+        .attr("x", function(d) { return x(d.x0) })
+        .attr("width", function(d) { return x(d.x1 - d.x0) })
+        .attr("y", function(d) { return 0 })
+        .attr("height", function(d) { return height })
+        .attr("class", function(d,i) { return i%2==0 ? "even" : "odd" })
+
+    // line generator
+    var line = d3.svg.line()
+      .x(function(d) { return x(xValue(d)); })
+      .y(function(d) { return y(yValue(d)); })
+
+
+    // Add the line path.
+    svg.append("svg:path")
+        .attr("class", "line")
+        .attr("clip-path", "url(#clip)")
+        .attr("d", line(records));
+
+    // Add the x-axis.
+    svg.append("svg:g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the y-axis.
+    svg.append("svg:g")
+        .attr("class", "y axis")
+        .call(yAxis);
 }
-
-function altitudeGraph(records, width, height, by_distance = true) { 
-
-}
-
-function lapTable(laps) {
-
-}
-
-
